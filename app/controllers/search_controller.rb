f4d0df33370633eb33888eff6a5998cbb6841aee
Simge Şen <simge.sen@bil.omu.edu.tr>
2018-03-20 18:17:06 +0200

    search controller oluşturuldu #4

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,10 @@
+class SearchController < ApplicationController
+  def index
+  end
+
+  def search
+  end
+
+  def list
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'search#index'
+
+  get 'search/search'
+
+  get 'search/list'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
